142e5c26292c0fa935434062b8601e744f6bf79a
gusta <gustavoandres_r@hotmail.com>
2023-07-20 17:59:12 +0200

    Saludo al usuario

--- Peso_Altura.py
+++ Peso_Altura.py
@@ -2,3 +2,7 @@
 
 nombre = input("Por favor, ingresa tu nombre: ")
 
+# Mostramo saludo personalizado
+
+print("Hola, " + nombre + " ¡Bienvenido! Te pediremos una serie de información para calcular tu IMC")
+
